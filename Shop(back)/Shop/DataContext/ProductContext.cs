5c3ce87a409999c996613f782a0cd9643d8ef9f4
Yan <kopn48@mail.ru>
2020-07-29 10:24:26 +0300

    init

---
+++ Shop(back)/Shop/DataContext/ProductContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace Shop.DataContext
+{
+    public class ProductContext : DbContext
+    {
+        public ProductContext(DbContextOptions options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+    }
+}
