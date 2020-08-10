4b829efd99edfd674b43fad83854f0ed61b1f755
ян козыренко <kopn48@mail.ru>
2020-08-11 00:09:22 +0300

    add cart notification, ajax post queries

--- ShopBack/Shop/wwwroot/js/script.js
+++ ShopBack/Shop/wwwroot/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
     $('.filter_button').click(e =>
     {
         if($('.filter_button').hasClass('filter_active'))
@@ -10,31 +11,45 @@ $(document).ready(function () {
             e.currentTarget.classList.add('filter_active');
         }
     });
-    $('.action_button').click(e =>
-    {
-        if(!$(e.target).hasClass('product_delete')){
+    $('.action_button').click(e => {
+        if (!$(e.target).hasClass('product_delete')) {
             product = e.target.parentElement.parentElement.parentElement;
             var price = product.children[1].children[1].children[0];
             var productTotal = product.children[2].children[1];
             var quantity = product.children[2].children[0].children[1];
         }
-        else{
+        else {
         }
     });
-    // $('.counter_input').keyup(e =>
-    // {
-    //     var key = e.originalEvent.key;
-    //     var input = e.target;
-    //     var value = input.value;
-    //     var rep = /[-\.;`\[\]/\?\*\(\)\&\^\%\$\#\@\!":'a-zA-Zа-яА-Я]/; 
-    //     console.log(input);
-    //     if(rep.test(value))
-    //     {
-    //         value = value.replace(rep, ''); 
-    //         input.value = value;
-    //     }
-    // }); 
+    $('.counter_submit').click(e => {
+        var values = { productId: window.location.pathname.split('/')[3], productQuantity: $('.counter_input')[0].value }
+        $.post({
+            type: "POST",
+            url: "",
+            data: values,
+            success: function (data) {
+                addItemNotification();
+
+            },
+        });
+    });
     function countAll(e)
     {
+    }
+    function addItemNotification() {
+        var wrapper = document.createElement('li');
+        wrapper.classList.add("notifications_list__item");
+        wrapper.innerHTML = 
+            `<div class="greendot_container"></div>
+            <span>Товар добавлен в корзину</span>`;
+        var list = document.querySelector(".notifications_list");
+        list.prepend(wrapper);
+        $(wrapper).animate({ opacity: 1 }, 200, "linear", setTimeout(function () {
+            $(wrapper).animate({ opacity: 0 }, 200, "linear", null);
+            setTimeout(function () {
+                wrapper.remove();
+            },500)
+        }, 4000));
+        
     }
 });
--- ShopFront/js/script.js
+++ ShopFront/js/script.js
@@ -10,31 +10,29 @@ $(document).ready(function () {
             e.currentTarget.classList.add('filter_active');
         }
     });
-    $('.action_button').click(e =>
-    {
-        if(!$(e.target).hasClass('product_delete')){
+    $('.action_button').click(e => {
+        if (!$(e.target).hasClass('product_delete')) {
             product = e.target.parentElement.parentElement.parentElement;
             var price = product.children[1].children[1].children[0];
             var productTotal = product.children[2].children[1];
             var quantity = product.children[2].children[0].children[1];
         }
-        else{
+        else {
         }
     });
-    // $('.counter_input').keyup(e =>
-    // {
-    //     var key = e.originalEvent.key;
-    //     var input = e.target;
-    //     var value = input.value;
-    //     var rep = /[-\.;`\[\]/\?\*\(\)\&\^\%\$\#\@\!":'a-zA-Zа-яА-Я]/; 
-    //     console.log(input);
-    //     if(rep.test(value))
-    //     {
-    //         value = value.replace(rep, ''); 
-    //         input.value = value;
-    //     }
-    // }); 
+    $('.counter_submit').click(e => {
+        var values = { productId: window.location.pathname.split('/')[3], productQuantity: $('.counter_input')[0].value }
+        $.post({
+            type: "POST",
+            url: "",
+            data: values,
+            success: values,
+        });
+    });
     function countAll(e)
     {
+    }
+    function addItemNotification () {
+
     }
 });
